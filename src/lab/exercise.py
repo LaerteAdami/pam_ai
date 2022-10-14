f35840a3e223c0cf318b60bd89c67bef6a67c545
Laerte Adami <laerte.adami@city.ac.uk>
2022-10-14 12:43:05 +0100

    First commit

--- src/lab/exercise.py
+++ src/lab/exercise.py
@@ -1,1 +1,16 @@
+def simple_math(a,b):
+    """
+    Just simple math
+    """
+    return a + b
+
+class broken_calculator(small_err):
+
+    def __init__(self):
+        
+        self.small_error = small_err
+
+    def wrong_math(self, a, b,):
+
+        return self.small_error + a + b   
 
