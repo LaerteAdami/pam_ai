cf50827d322be3521d54a763f9c16c255d139c5c
Laerte Adami <laerte.adami@city.ac.uk>
2022-10-16 15:23:07 +0100

    New file

--- src/lab/exercise.py
+++ src/lab/exercise.py
@@ -1,18 +1,23 @@
-def simple_math(a,b):
+def simple_math(a, b):
     """
     Just simple math
     """
     return a + b
 
-class broken_calculator():
+
+class broken_calculator:
     """
     Class for compute wrong solutions
     """
-    def __init__(self,small_err):
-        
+
+    def __init__(self, small_err):
+
         self.small_error = small_err
 
-    def wrong_math(self, a, b,):
+    def wrong_math(
+        self,
+        a,
+        b,
+    ):
 
-        return self.small_error + a + b   
-
+        return self.small_error + a + b
--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,7 +1,8 @@
 # some_file.py
 import sys
+
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '/Users/laerte/pam_ai/pam_ai/src/lab')
+sys.path.insert(1, "/Users/laerte/pam_ai/pam_ai/src/lab")
 
 import exercise as ex
 
@@ -12,8 +13,8 @@ BR = ex.broken_calculator(3)
 a = 1
 b = 2
 
-correct_sum = ex.simple_math(a,b)
+correct_sum = ex.simple_math(a, b)
 
-wrong_sum = BR.wrong_math(a,b)
+wrong_sum = BR.wrong_math(a, b)
 
-print("Correct: {}, wrong: {}".format(correct_sum,wrong_sum))
+print("Correct: {}, wrong: {}".format(correct_sum, wrong_sum))
