c9d7c672b62a499891b458e5c08e26adcfb5a606
Laerte Adami <laerte.adami@city.ac.uk>
2022-10-21 11:44:50 +0100

    Add setup

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+from os import path
+from setuptools import find_packages, setup
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+this_directory = path.abspath(path.dirname(__file__)) 
+
+
+setup(name='pam_ai',
+      version='1.0',
+      description='Programming and Mathematics for AI - Coursework',
+      author='Laerte Adami',
+      author_email='laerte.adami@city.ac.uk',
+      url='https://github.com/LaerteAdami/pam_ai',
+      packages=find_packages(where="src"),
+      package_dir={"":"src"}
+      #include_package_data=True,
+      #install_requires = requirements,
+     )
