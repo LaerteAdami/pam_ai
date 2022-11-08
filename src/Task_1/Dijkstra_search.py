79893ec35ad5c59d60f37ab0f34c5df857c30bde
Laerte Adami <laerte.adami@city.ac.uk>
2022-11-08 16:13:11 +0000

    modified Dijkstra_search_v2 into Dijkstra_search and add plot to notebook1

--- src/Task_1/Dijkstra_search.py
+++ src/Task_1/Dijkstra_search.py
@@ -3,8 +3,9 @@ class DijkstraSearch():
     def __init__(self, agent, random_grid):
         self.agent = agent
         self.grid = random_grid.grid
-        self.w = random_grid.grid.shape[1]
-        self.h = random_grid.grid.shape[0]
+        self.w = random_grid.grid.shape[1] - 1
+        self.h = random_grid.grid.shape[0] - 1
+        self.count_steps = 0
 
     def compute_path(self):
         """
@@ -18,57 +19,66 @@ class DijkstraSearch():
         import time
 
         start = time.time() # variable used to store the initial time. 
-                            # It will be use to compute the total execution time of the search
+                            # It will be use to compute the total execution time of the search                                                                                                                                                                        
 
-       # Initiate variables
-        unvisited_set = set([ (i,j) for i in range(self.h) for j in range(self.w)])
+        target = (self.h, self.w) # target cell, i.e., the bottom right one 
+
+        # Initialise variables 
+        unvisited_list = [ (i,j) for i in range(self.h+1) for j in range(self.w+1)] # list containing all the nodes to be visited
         distance_matrix = float("inf") * np.ones_like(self.grid)  # type: ignore
+
+        current_node = (0,0) # initial location set as current node
+        distance_matrix[current_node] = 0 # set distance 0 for node (0,0)
+        visited_set = {current_node} # mark (0,0) as visited
+        unvisited_list.remove(current_node) # remove (0,0) from the unvisited nodes
+
+        self.agent.timer += self.grid[current_node]
+        counter = 0
+
+        while target not in visited_set: # loop until the target is visited
         
-        # Set current node to (0,0)
-        current_node = (0,0)
-        distance_matrix[current_node] = 0
-        self.agent.timer = self.grid[0,0] # initialise the timer of the agent
-        
-        # Initialise sets
-        visited_set = {current_node}
-        unvisited_set.remove(current_node)
+            neighbours = self.get_neighbours(current_node,visited_set) # get neighbour nodes 
 
-        stop_condition = False
+            while len(neighbours)!=0: # loop for every neighbour
+                temp_node = neighbours.pop()
+                if distance_matrix[temp_node] > self.grid[temp_node] + distance_matrix[current_node]: # update distance matrix with minimum values from neighbours
+                    distance_matrix[temp_node] = self.grid[temp_node] + distance_matrix[current_node]
 
-        while not stop_condition:
+            distance_matrix_unvisited = [distance_matrix[node] for node in unvisited_list] # array with the values of all 
+            next_node = unvisited_list[distance_matrix_unvisited.index(np.min(distance_matrix_unvisited))] # minimum distance for all unvisited nodes
 
-            i = current_node[0]
-            j = current_node[1]
-            neighborhood = list()
+            visited_set.add(next_node)  # type: ignore # mark next node as visited
+            unvisited_list.remove(next_node) # remove next node to the unvisited list
+            #visited_list.append(((current_node),(next_node)))   # type: ignore
+            current_node = next_node # set the next node as the current
+
+            counter +=1
+            #if counter == 1000:
+            #    break
             
-            if (i+1,j) in unvisited_set: self.update_distance(distance_matrix,i+1, j, current_node); neighborhood.append((i+1,j))
-            if (i,j+1) in unvisited_set: self.update_distance(distance_matrix,i, j+1, current_node); neighborhood.append((i,j+1))
-            #if j != (self.w-1) and (i-1,j) in unvisited_set: self.update_distance(distance_matrix, i-1, j, current_node); neighborhood.append((i-1,j))
-            #if i != (self.h-1) and (i,j-1) in unvisited_set: self.update_distance(distance_matrix, i, j-1, current_node); neighborhood.append((i,j-1))
-
-            find_min = list()
-            for k in neighborhood:
-                find_min.append(distance_matrix[k])
-
-            next_node = neighborhood[find_min.index(min(find_min))]    
-
-            unvisited_set.remove(next_node)
-            visited_set.add(next_node)   # type: ignore 
-            current_node = next_node
-            stop_condition = current_node == (self.h-1,self.w-1)
-
-            self.agent.timer += self.grid[next_node]
+        self.agent.timer += int(distance_matrix[target])    
 
         self.visited_set = visited_set 
         self.agent.execution_time = (time.time() - start ) * 1000 # compute execution time in ms
+        self.count_steps = counter
 
-    def update_distance(self, distance_matrix, i, j, current_node):
+    def get_neighbours(self, current_node, visited_set):
         """
-        update_distance is used to updated the distance_matrix with new distance values, smaller than
-        the previous ones
+        This function finds the neighbour cells given the current cell. 
+        Only unvisisted neighbours are considered
 
         """
-        if (j > self.w   or i > self.h or i < 0 or j < 0 ):
-            return  
-        distance_matrix[i,j] = self.grid[i,j] + distance_matrix[current_node]   
-        return distance_matrix    
+
+        neighbours = list() # initialise the output
+        i = current_node[0] # set vertical coordinate
+        j = current_node[1] # set horizontal coordinate
+
+        if i + 1 in range(self.h+1) and (i+1,j) not in visited_set:
+            neighbours.append((i+1,j))
+        if i - 1 in range(self.h+1) and (i-1,j) not in visited_set:
+            neighbours.append((i-1,j))
+        if j + 1 in range(self.w+1) and (i,j+1) not in visited_set:
+            neighbours.append((i,j+1))
+        if j - 1 in range(self.w+1) and (i,j-1) not in visited_set:
+            neighbours.append((i,j-1))    
+        return neighbours    
